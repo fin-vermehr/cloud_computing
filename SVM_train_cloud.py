b9667ed1c2dd8bf1be6d994da9af2f417d1483e2
Fin Vermehr <fin.vermehr@gmail.com>
2018-08-08 21:51:18 -0700

    added data processing

--- SVM_train_cloud.py
+++ SVM_train_cloud.py
@@ -44,7 +44,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y_enc
 )
 
-clf = svm.LinearSVC(loss='hinge')
+clf = svm.LinearSVC(loss='hinge', C=1)
 clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
@@ -56,30 +56,30 @@ print(pd.DataFrame(
       index=[['actual', 'actual'], ['spam', 'ham']],
       columns=[['predicted', 'predicted'], ['spam', 'ham']]))
 
-param_grid = [{'C': np.logspace(-4, 4, 15)}]
+# param_grid = [{'C': np.logspace(-4, 4, 15)}]
+#
+# grid_search = GridSearchCV(
+#     estimator=svm.LinearSVC(loss='hinge'),
+#     param_grid=param_grid,
+#     cv=StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=42),
+#     scoring='f1',
+#     n_jobs=-1
+# )
 
-grid_search = GridSearchCV(
-    estimator=svm.LinearSVC(loss='hinge'),
-    param_grid=param_grid,
-    cv=StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=42),
-    scoring='f1',
-    n_jobs=-1
-)
 
-
-grid_search.fit(X_ngrams, y_enc)
-final_clf = svm.LinearSVC(loss='hinge', C=grid_search.best_params_['C'])
-final_clf.fit(X_ngrams, y_enc)
-y_pred = final_clf.predict(X_test)
-
-print('Best Parameter:' + str(grid_search.best_params_['C']))
-
-print(metrics.f1_score(y_test, y_pred))
-
-print(pd.DataFrame(
-      metrics.confusion_matrix(y_test, y_pred),
-      index=[['actual', 'actual'], ['spam', 'ham']],
-      columns=[['predicted', 'predicted'], ['spam', 'ham']]))
+# grid_search.fit(X_ngrams, y_enc)
+# final_clf = svm.LinearSVC(loss='hinge', C=grid_search.best_params_['C'])
+# final_clf.fit(X_ngrams, y_enc)
+# y_pred = final_clf.predict(X_test)
+#
+# print('Best Parameter:' + str(grid_search.best_params_['C']))
+#
+# print(metrics.f1_score(y_test, y_pred))
+#
+# print(pd.DataFrame(
+#       metrics.confusion_matrix(y_test, y_pred),
+#       index=[['actual', 'actual'], ['spam', 'ham']],
+#       columns=[['predicted', 'predicted'], ['spam', 'ham']]))
 
 
 print(pd.Series(
@@ -91,31 +91,28 @@ print(pd.Series(
       index=vectorizer.get_feature_names()).sort_values(ascending=True)[:20])
 
 
-# df_final = pd.DataFrame()
-#
-# df_input = pd.read_csv('prepocessed_input.csv')
-#
-# score_list = []
-# username_list = []
-# category_list = []
-#
-#
-# for index in df_input.index:
-#     username = df_input['username'].loc[index]
-#     tweet = df_input['tweet'].loc[index]
-#     score = final_clf.decision_function(vectorizer.transform(tweet))
-#
-#     if final_clf.predict(vectorizer.transform(tweet)):
-#         category_list.append('r')
-#     else:
-#         category_list.append('o')
-#
-#     username_list.append(username)
-#     score_list.append(score)
-#
-# df_final['username'] = username_list
-# df_final['score'] = score_list
-# df_final['category'] = category_list
-#
-# df_final.to_csv('categorized_users.csv')
-#
+df_final = pd.DataFrame()
+
+df_input = pd.read_csv('prepocessed_input.csv')
+
+df_list = list()
+for index in df_input.index:
+    df_tmp = pd.DataFrame()
+
+    username = df_input['username'].loc[index]
+    tweet = df_input['tweet'].loc[index]
+    score = clf.decision_function(vectorizer.transform(tweet))
+
+    df_tmp['username'] = username
+    df_tmp['score'] = score
+
+    if clf.predict(vectorizer.transform(tweet)):
+        df_tmp['category'] = 'r'
+    else:
+        df_tmp['category'] = 'o'
+
+    df_list.append(df_tmp)
+
+df_final = df_final.concat(df_list)
+
+df_final.to_csv('categorized_users.csv')
